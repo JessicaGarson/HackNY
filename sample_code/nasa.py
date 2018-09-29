5593482fe78a29a7121751b5a6800566253d166c
JessicaGarson <jessica.garson@gmail.com>
2018-09-29 12:24:31 -0400

    adding

--- sample_code/nasa.py
+++ sample_code/nasa.py
@@ -5,5 +5,5 @@ def nasa_image():
     url = 'https://api.nasa.gov/planetary/apod'
     params = {'api_key': 'DEMO_KEY'}
     response = requests.get(url, params).json()
-    image = response['url']
+    image = response['hdurl']
     return image
