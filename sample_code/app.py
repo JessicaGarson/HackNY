8316d95d19ee32d3508bbfe0e4cc9e5baac225c0
JessicaGarson <jessica.garson@gmail.com>
2018-09-29 10:57:47 -0400

    sample code

---
+++ sample_code/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+from nasa import nasa_image
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def display():
+    picture = nasa_image()
+    format_pict = '<h1>Nasa Image of the Day</h1> <img src="{}" alt="" style="width:400px; height:400px;">'.format(
+        picture)
+    return format_pict
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
---
+++ sample_code/nasa.py
@@ -0,0 +1,9 @@
+import requests
+
+
+def nasa_image():
+    url = 'https://api.nasa.gov/planetary/apod'
+    params = {'api_key': 'DEMO_KEY'}
+    response = requests.get(url, params).json()
+    image = response['url']
+    return image
